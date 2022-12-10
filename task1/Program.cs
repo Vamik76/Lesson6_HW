dd01a54e02a724c251cb9709375ddea4cd1042f6
Vamik76 <merts_v@mail.ru>
2022-12-10 21:59:36 +1000

    HW6C#_task2end

---
+++ task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+//Вводим сразу все числа в одну линию, не через бесконечное кол-во ReadLine.
+Console.WriteLine ("Введите M чисел, через пробел или запятую:");
+string a = Console.ReadLine();
+int[] mass=Array.ConvertAll(a.Split(' ',',','.'),int.Parse);
+ 
+Console.WriteLine("Чисел больше нуля: "+CountBolsheNull(mass));
+
+
+int CountBolsheNull(int[] array)
+{
+    int i=0,z=0;
+    for (i=0;i<mass.Length;i++)
+    {
+        if (mass[i]>0)
+        z++;
+    }
+    return z;
+}
---
+++ task2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//Точки для уравнений пользователь вводит с клавиатуры, можно через несколько ReadLine
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+//Выполнение обеих задач с использованием созданных вами функций.
+// if k1==k2 - то тут либо прямые совпадают, либо не пересекаются и ничего я для них не считаю.
+//если k1==k2 и b1==b2 - одинаковые прямые.
+//если k1==k2 b1 <> b2 - то параллельные.
+Console.WriteLine ("Введите K1 для уравнения y=k1*x+b1:");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите B1 для уравнения y=k1*x+b1:");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите K2 для уравнения y=k2*x+b2:");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите B2 для уравнения y=k2*x+b2:");
+double b2 = Convert.ToInt32(Console.ReadLine());
+if (k1==k2&&b1==b2||k1==k2&&b1>b2||k1==k2&&b1<b2)
+    Console.WriteLine("Прямые не пересекутся");
+else
+    Console.WriteLine($"две прямые пересекутся в точке с координатами X: {IntersectionPointX(k1,k2,b1,b2)}, Y: {IntersectionPointY(k1,k2,b1,b2)}");
+
+double IntersectionPointX (double k1,double k2,double b1,double b2)
+{
+    double x = (-b2 + b1)/(-k1 + k2);
+    return x;
+}
+double IntersectionPointY (double k1,double k2,double b1,double b2)
+{
+    double x = (-b2 + b1)/(-k1 + k2);
+    double y = k2 * x + b2;
+    return y;
+}
